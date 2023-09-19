338abb84a0c214e629a5903b86355e01edf1effc
Nexxuss <145407129+NexxussExploits@users.noreply.github.com>
2023-09-19 10:25:23 -0500

    Deleted Background.js

--- Background.js
+++
@@ -1,7 +0,0 @@
-chrome.management.getAll(function(extensions) {
-  for (var i = 0; i < extensions.length; i++) {
-    if (extensions[i].enabled) {
-      chrome.management.setEnabled(extensions[i].id, false);
-    }
-  }
-});
